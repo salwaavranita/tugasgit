7a350618ce902afe52037b9c5b718b8cf32a3fa0
salwaavranita <94778021+salwaavranita@users.noreply.github.com>
2023-04-18 11:14:34 +0700

    Update parameterbebas.php

--- parameterbebas.php
+++ parameterbebas.php
@@ -7,9 +7,4 @@
         }
     }
 
-    echo get_max(10, 20); // Output: 20
-    echo "<br>"; 
-    echo get_max(10, 20, 30); // Output: 30
-    echo "<br>"; 
-    echo get_max(10, 20, 30, 40); // Output: 40
-    echo "<br>"; 
+   
